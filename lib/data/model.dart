59c0fdfa41590b490c89172edd3ae7bd54cb132e
Thai Parkerizing <administrator@192.168.1.34>
2022-03-09 07:55:50 +0700

    .

---
+++ lib/data/model.dart
@@ -0,0 +1,177 @@
+class dataset {
+  dataset({
+    this.id = 0,
+    this.f01 = '',
+    this.f02 = '',
+    this.f03 = '',
+    this.f04 = '',
+    this.f05 = '',
+    this.f06 = '',
+    this.f07 = '',
+    this.f08 = '',
+    this.f09 = '',
+    this.f10 = '',
+    this.f11 = '',
+    this.f12 = '',
+    this.f13 = '',
+    this.f14 = '',
+    this.f15 = '',
+    this.f16 = '',
+    this.f17 = '',
+    this.f18 = '',
+    this.f19 = '',
+    this.f20 = '',
+    this.f21 = '',
+    this.f22 = '',
+    this.f23 = '',
+    this.f24 = '',
+    this.f25 = '',
+    this.f26 = '',
+    this.f27 = '',
+    this.f28 = '',
+    this.f29 = '',
+    this.f30 = '',
+    this.f31 = '',
+    this.f32 = '',
+    this.f33 = '',
+    this.f34 = '',
+    this.f35 = '',
+    this.f36 = '',
+    this.f37 = '',
+    this.f38 = '',
+    this.f39 = '',
+    this.f40 = '',
+    this.f41 = '',
+    this.f42 = '',
+    this.f43 = '',
+    this.f44 = '',
+    this.f45 = '',
+    this.f46 = '',
+    this.f47 = '',
+    this.f48 = '',
+    this.f49 = '',
+    this.f50 = '',
+    this.f51 = '',
+    this.f52 = '',
+    this.f53 = '',
+    this.f54 = '',
+    this.f55 = '',
+    this.f56 = '',
+    this.f57 = '',
+    this.f58 = '',
+    this.f59 = '',
+    this.f60 = '',
+    this.f61 = '',
+    this.f62 = '',
+    this.f63 = '',
+    this.f64 = '',
+    this.f65 = '',
+    this.f66 = '',
+    this.f67 = '',
+    this.f68 = '',
+    this.f69 = '',
+    this.f70 = '',
+    this.f71 = '',
+    this.f72 = '',
+    this.f73 = '',
+    this.f74 = '',
+    this.f75 = '',
+    this.f76 = '',
+    this.f77 = '',
+    this.f78 = '',
+    this.f79 = '',
+    this.f80 = '',
+  });
+  int id;
+  String f01;
+  String f02;
+  String f03;
+  String f04;
+  String f05;
+  String f06;
+  String f07;
+  String f08;
+  String f09;
+  String f10;
+
+  String f11;
+  String f12;
+  String f13;
+  String f14;
+  String f15;
+  String f16;
+  String f17;
+  String f18;
+  String f19;
+  String f20;
+
+  String f21;
+  String f22;
+  String f23;
+  String f24;
+  String f25;
+  String f26;
+  String f27;
+  String f28;
+  String f29;
+  String f30;
+
+  String f31;
+  String f32;
+  String f33;
+  String f34;
+  String f35;
+  String f36;
+  String f37;
+  String f38;
+  String f39;
+  String f40;
+
+  String f41;
+  String f42;
+  String f43;
+  String f44;
+  String f45;
+  String f46;
+  String f47;
+  String f48;
+  String f49;
+  String f50;
+
+  String f51;
+  String f52;
+  String f53;
+  String f54;
+  String f55;
+  String f56;
+  String f57;
+  String f58;
+  String f59;
+  String f60;
+
+  String f61;
+  String f62;
+  String f63;
+  String f64;
+  String f65;
+  String f66;
+  String f67;
+  String f68;
+  String f69;
+  String f70;
+
+  String f71;
+  String f72;
+  String f73;
+  String f74;
+  String f75;
+  String f76;
+  String f77;
+  String f78;
+  String f79;
+  String f80;
+
+  bool selected = false;
+}
+
+List<dataset> ZeroDataset = [];
